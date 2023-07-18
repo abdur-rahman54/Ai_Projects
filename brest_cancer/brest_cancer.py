59c4929a182c068e95aec145132ee0cb2374dee2
abdur-rahman54 <abdur.rahman59354@gmail.com>
2023-07-18 13:09:27 -0400

    Using Statsmodels Formulas and Sklearn Linear Model

--- brest_cancer/brest_cancer.py
+++ brest_cancer/brest_cancer.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import statsmodels.formula.api as smf
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 cancer1 = load_breast_cancer()
 cancer = pd.DataFrame(cancer1.data, columns = cancer1.feature_names)
@@ -23,7 +24,9 @@ y = cancer.Target
 # Split these data into training and testing datasets
 x_train, x_test, y_train, y_test = train_test_split(x,y, random_state=123) 
 
-print("\nMethod 1:\n")
+
+
+print("\nMethod 1: Using Statsmodels Formulas\n")
 
 # Create the formula string 
 all_columns = ' + '.join(cancer.columns[:10])
@@ -46,9 +49,32 @@ print ("\nConfusion Matrix : \n", cm)
 
 # Accuracy score
 print('\nTest accuracy = ', accuracy_score(y_test, prediction_1))
-print("")
+score1 = accuracy_score(y_test, prediction_1)
 
-print("\nMethod 2:\n")
+
+
+print("\nMethod 2: Sklearn Linear Model\n")
+
+log_reg_2 = LogisticRegression(penalty=None, solver = 'newton-cg', max_iter= 150).fit(x_train,y_train)
+
+# Predict responses 
+pred_2 = log_reg_2.predict(x_test)
+prediction_2 = list(map(round, pred_2))
+
+# Confusion matrix
+cm = confusion_matrix(y_test, prediction_2) 
+print ("\nConfusion Matrix : \n", cm) 
+
+# Accuracy score
+print('\nTest accuracy = ', accuracy_score(y_test, prediction_2))
+score2 = accuracy_score(y_test, prediction_2)
+
+if score1 == score2:
+	print("\nBoth method's accuracy is same")
+else:
+	print("\nResults does not matches with eatch other")
+
+
 
 
 
